fed5903a07b7695b1f42bcee4f6f1d9683af0b89
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-07-20 16:22:09 +0900

    scafollding

---
+++ src/App.vue
@@ -0,0 +1,54 @@
+<template>
+  <div id="app">
+    <form v-if="isSearch">
+      <input ref="inputSearch" type="text" placeholder="search..." />
+    </form>
+    <button @click="onClickSearchToggle">검색하기</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      isSearch: false
+    }
+  },
+  methods: {
+    onClickSearchToggle () {
+      this.isSearch = !this.isSearch
+      this.$refs.inputSearch.focus()
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
